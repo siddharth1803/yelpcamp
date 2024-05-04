0d4282a6b1fc39da13299121bc4283d7a1a47284
siddharth <shobhitsingh1803@gmail.com>
2024-05-04 18:45:49 +0530

    start script

--- app.js
+++ app.js
@@ -103,6 +103,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log("Server Running")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
