1d6a1065403d592974f215851ccdfe932233205d
siddharth <shobhitsingh1803@gmail.com>
2024-05-04 21:54:44 +0530

    seeds changes

--- seeds/index.js
+++ seeds/index.js
@@ -16,7 +16,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 250; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const random6 = Math.floor(Math.random() * images.length);
         const price = Math.floor(Math.random() * 20) + 10;
